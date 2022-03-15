0c9564bc6ea64885a852de7da4f7a220cdc2e740
Mohammad Hamza Ibrahim <mi469@live.mdx.ac.uk>
2022-03-15 14:06:52 +0000

    edited submit

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -131,7 +131,7 @@ export default {
     },
 
     submit() {
-      alert(`Dear ${this.name}, your order has been submitt`);
+      alert(`Dear ${this.name}, your order has been submit`);
       location.reload();
     }
   }
